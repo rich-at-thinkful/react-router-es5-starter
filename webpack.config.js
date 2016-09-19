cf495bb342fc5bce6a6175b237b00431eea6eed5
Rich Greenhill <rgnet1@yahoo.com>
2016-09-19 15:55:50 -0700

    config for dev/prod

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 var path = require('path');
+var webpack = require('webpack');
+var shouldUglify = process.env.uglify === '1';
 
 module.exports = {
   context: path.join(__dirname, "src"),
@@ -12,6 +14,17 @@ module.exports = {
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
     ]
   },
-  devtool: 'source-map'
+  devtool: shouldUglify ? '' : 'source-map',
+  plugins: shouldUglify ? [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      comments: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  ] : []
 };
 
